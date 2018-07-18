274c16193c9d76626e700fd241579af2f771c63c
shehryarahmed <shehreyarahmed5@gmail.com>
2018-07-18 17:30:50 +0500

    working on firebase instance

--- Instagram/app/src/main/java/com/example/tx/instagram/MainActivity.java
+++ Instagram/app/src/main/java/com/example/tx/instagram/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tx.instagram;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -12,9 +14,12 @@ import android.view.MenuItem;
 import com.example.tx.instagram.Home.CameraFragment;
 import com.example.tx.instagram.Home.HomeFragment;
 import com.example.tx.instagram.Home.MessageFragment;
+import com.example.tx.instagram.Login.LoginActivity;
 import com.example.tx.instagram.utils.BottomNavigationViewHelper;
 import com.example.tx.instagram.utils.SectionPagerAdapter;
 import com.example.tx.instagram.utils.UniversalImageLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -23,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     public static final int ACTIVITY_NUM = 0;
     private Context mContext = MainActivity.this;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: run");
 
+        setUpFirebaseAuth();
         initImageLoader();
         setUpBottomNavigationView();
         setUpViewPager();
@@ -67,5 +76,54 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_icon_instagram);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_send);
+    }
+        /*
+        ***********************************************Firebase******************************************************
+         */
+    /**
+     * setup Firebase auth object
+     */
+
+    private void checkCurrentUser(FirebaseUser user){
+        Log.d(TAG, "checkCurrentUser: checking if user is logging in.");
+        if (user == null){
+            Intent intent = new Intent(mContext, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+    private void setUpFirebaseAuth(){
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListner =  new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                //check if user is logged in
+                checkCurrentUser(user);
+                if(user != null){
+                    Log.d(TAG, "onAuthStateChanged: "+user.getUid());
+                }
+                else{
+                    Log.d(TAG, "signout: ");
+                }
+            }
+        };
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListner);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(mAuthListner != null){
+            mAuth.removeAuthStateListener(mAuthListner);
+        }
     }
 }
